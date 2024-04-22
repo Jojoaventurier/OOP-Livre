27e501c18d646369731ab90e2673913520f20c4a
Geoffroy Maurutto <geoffroy.maurutto@gmail.com>
2024-04-22 14:41:00 +0200

    samll updt

--- classes/Genre.php
+++ classes/Genre.php
@@ -2,8 +2,39 @@
 
 class Genre {
 
-    private int $id_genre;
-    private string $genre_name;
+    private int $idGenre;
+    private string $genreName;
 
-    
+    public function __construct(int $idGenre, string $genreName) {
+        $this->idGenre = $idGenre;
+        $this->genreName = $genreName;
+    }
+
+    public function getIdGenre()
+    {
+        return $this->idGenre;
+    }
+
+    public function setIdGenre($idGenre)
+    {
+        $this->idGenre = $idGenre;
+
+        return $this;
+    }
+
+    public function getGenreName()
+    {
+        return $this->genreName;
+    }
+
+    public function setGenreName($genreName)
+    {
+        $this->genreName = $genreName;
+
+        return $this;
+    }
+
+    public function __toString() {
+        return $this->genreName ." ";
+    }
 }
