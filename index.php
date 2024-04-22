bfa66a9a6a5e60ac482ea9682420cb677e0de72d
Geoffroy Maurutto <geoffroy.maurutto@gmail.com>
2024-04-22 15:55:28 +0200

    initialized index file with the require queries

--- index.php
+++ index.php
@@ -1,5 +1,12 @@
+<?php
+
+/*The include and require statements are identical, except upon failure:
+	require will produce a fatal error (E_COMPILE_ERROR) and stop the script
+    include will only produce a warning (E_WARNING) and the script will continue*/
+
+    require "/classes/Author.php";
+    require "/classes/Book.php";
+    require "/classes/Genre.php";
 
 
-require et include diff 
-
-cree les objets
+    
